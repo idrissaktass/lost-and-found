ebfef3085b759e61d9e7ecfec882b3dd9241d858
idrissaktass <idrissaktass98@gmail.com>
2024-10-10 00:05:10 +0300

    update

--- backend/api/messages/[username]/[recipient].js
+++ backend/api/messages/[username]/[recipient].js
@@ -1,6 +1,6 @@
 import dbConnect from '../../../utils/dbConnect';
 import Message from '../../../models/Message';
-import User from '../../../models/User';  // Ensure User is imported
+import User from '../../../models/User';
 import Cors from 'cors';
 
 const cors = Cors({
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'https://lost-and-found-frontend-mu.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    return res.status(204).end(); // No content for OPTIONS method
+    return res.status(204).end();
   }
 
   await dbConnect();
@@ -43,6 +43,7 @@ export default async function handler(req, res) {
 
     if (!user || !recipientUser) {
       console.error('User or Recipient not found');
+      res.setHeader('Access-Control-Allow-Origin', 'https://lost-and-found-frontend-mu.vercel.app');
       return res.status(404).json({ error: 'User not found' });
     }
 
@@ -55,9 +56,11 @@ export default async function handler(req, res) {
     .populate('sender', 'username')
     .populate('recipient', 'username');
 
+    res.setHeader('Access-Control-Allow-Origin', 'https://lost-and-found-frontend-mu.vercel.app');
     res.status(200).json(messages);
   } catch (error) {
     console.error("Error fetching messages:", error);
+    res.setHeader('Access-Control-Allow-Origin', 'https://lost-and-found-frontend-mu.vercel.app');
     res.status(500).json({ error: 'Failed to fetch messages' });
   }
 }
