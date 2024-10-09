72342184ff19ce673784887275f46901606c06ee
idrissaktass <idrissaktass98@gmail.com>
2024-10-10 00:20:51 +0300

    update

--- backend/api/messages/index.js
+++ backend/api/messages/index.js
@@ -2,5 +2,5 @@
 export { default as send } from './send';
 export { default as read } from './read';
 export { default as getAllMessages } from './getAllMessages'; // Get all messages for a user
-export { default as getMessagesBetween } from './getMessagesBetween'; // Get messages between two users
+export { default as getMessagesBetween } from './[username]/[recipient]'; // Get messages between two users
 export { default as getUnread } from './getUnread';
