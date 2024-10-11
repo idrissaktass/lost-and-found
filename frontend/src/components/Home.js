d791b4cc51b964b4f4e6790e4916d5f59c643ebe
idrissaktass <idrissaktass98@gmail.com>
2024-10-11 12:01:45 +0300

    update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -44,43 +44,6 @@ const Home = () => {
         <Grid height={"100vh"} overflow={"hidden"}>
             <Navbar />
             <Box flexGrow={1}>
-            <Box flexGrow={1} sx={{
-                content: '""',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundImage: 'url(/items.png), url(/items.png), url(/pets.png), url(/pets.png)',
-                backgroundSize: {
-                xs: '50% 25%',
-                sm: '25% 25%',
-                md: '30% 25%',
-                lg: '25% 25%',
-                },
-                backgroundPosition: {
-                xs: '0 0, 0 0, 100% 0, 100% 0',
-                sm: '0 0, 30% 0, 70% 0, 100% 0',
-                md: '0 0, 41.2% 0, 60% 0, 100% 0',
-                lg: '0 0, 32% 0, 68% 0, 100% 0',
-                },
-                backgroundRepeat: 'repeat-y, repeat-y, repeat-y, repeat-y',
-                backgroundColor: 'rgba(255, 255, 255, 0)',
-                '&::before': {
-                content: '""',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundImage: 'url(/items.png), url(/items.png), url(/pets.png), url(/pets.png)',
-                backgroundSize: '50% 50%, 50% 50%',
-                backgroundPosition: 'left, left, right, right',
-                backgroundRepeat: 'repeat-y',
-                opacity: 0.1, 
-                zIndex: -1,
-                },
-            }}>
                 <Grid container justifyContent={"center"} pb={10}>
                     <Grid item size={{ xs: 12, md: 10, lg: 9 }} height={"91.8vh"} sx={{ background: 'linear-gradient(to right, #0088ff8c, #007fff2b)' }}
                         overflow={"auto"} boxShadow={"0px 5px 10px #b4b4b4"} px={{ xs: 1, sm: 3 }} py={3} className={`content ${showContent ? 'open' : 'closed'}`}>
@@ -163,7 +126,6 @@ const Home = () => {
                         )}
                     </Grid>
                 </Grid>
-                </Box>
             </Box>
         </Grid>
     );
