b86ef2c8bc635f3961dc1f0a99d4b53e357eb720
idrissaktass <idrissaktass98@gmail.com>
2024-10-17 22:30:10 +0300

    update

--- frontend/src/components/LostModal.js
+++ frontend/src/components/LostModal.js
@@ -139,7 +139,7 @@ const LostModal = ({ open, onClose }) => {
             backgroundSize: '50%',
             backgroundPosition: 'center',
             opacity: 0.1,
-            height:{xs:"110%", sm:0},
+            height:{xs:"110%", sm:"100%"},
             zIndex: -1,
           },
         }}
