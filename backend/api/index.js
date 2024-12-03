3036efff7fd61138cd40152382366288ca4dfca8
idrissaktass <80005089+idrissaktass@users.noreply.github.com>
2024-12-03 14:07:08 +0300

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,24 +6,21 @@ import { listingId } from './listing';
 
 const app = express();
 
-// CORS configuration
 const corsOptions = {
-    origin: 'https://lost-and-found-frontend-mu.vercel.app', // your frontend URL
+    origin: 'https://www.lostandfoundtr.online/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
 
-// Use CORS middleware
 app.use(cors(corsOptions));
-app.use(express.json()); // For parsing application/json
+app.use(express.json());
 
-// Define your routes
 app.post('/api/messages/send', send);
 app.get('/api/messages/getUnread', getUnread);
 app.get('/api/messages/:username/:recipient', getMessagesBetweenUsers);
-app.put('/api/messages/read/:messageId', read); // Use PUT for marking messages as read
-app.get('/api/lost/:id', lostId); // Ensure it's GET for fetching lost item by ID
-app.get('/api/listing/:id', listingId); // Ensure it's GET for fetching listing by ID
+app.put('/api/messages/read/:messageId', read);
+app.get('/api/lost/:id', lostId);
+app.get('/api/listing/:id', listingId);
 
 export default app;
