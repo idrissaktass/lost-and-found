3c62a3f1347055f2bd8147babc87dafda113ff0c
idrissaktass <80005089+idrissaktass@users.noreply.github.com>
2024-12-10 10:22:02 +0300

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ import { listingId } from './listing';
 const app = express();
 
 const corsOptions = {
-    origin: 'https://www.lostandfoundtr.online/',
+    origin: 'https://www.lostandfoundtr.online',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
